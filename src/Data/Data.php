913f0d0ccae120fdf1d310ff8f1faedf733d5e40
Danilo Andrade <mandrade.danilo@gmail.com>
2018-07-22 12:05:46 -0300

    Implement push method in data container

--- src/Data/Data.php
+++ src/Data/Data.php
@@ -313,7 +313,32 @@ class Data implements DataInterface, \JsonSerializable, \ArrayAccess, \IteratorA
      */
     public function push($path, $value)
     {
-        // TODO: Implement push() method.
+        $node = $this->get($path);
+
+        if (!$node) {
+            $node = [];
+        } elseif (is_object($node)) {
+            $node = get_object_vars($node);
+        }
+
+        if (!is_array($node)) {
+            throw new \UnexpectedValueException(sprintf('The value at path: %s should be object or array but is %s.',
+                $path, gettype($node)));
+        }
+
+        $args = func_get_args();
+
+        if (count($args) <= 2) {
+            $num = array_push($node, $value);
+        } else {
+            $args[0] = &$node;
+
+            $num = call_user_func_array('array_push', $args);
+        }
+
+        $this->set($path, $node);
+
+        return $num;
     }
 
     /**
