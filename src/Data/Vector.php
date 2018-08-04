e3170933e4d2449b50b0d4a0b0f5956b798d66f0
Danilo Andrade <mandrade.danilo@gmail.com>
2018-08-04 09:35:44 -0300

    add vector data class

---
+++ src/Data/Vector.php
@@ -0,0 +1,32 @@
+<?php
+namespace Apli\Data;
+
+/**
+ * Class Vector is a sequence of values in a contiguous buffer that grows and
+ * shrinks automatically.
+ * @package Apli\Data
+ */
+final class Vector implements \IteratorAggregate, \ArrayAccess, Sequence
+{
+    use Traits\GenericCollection;
+    use Traits\GenericSequence;
+    use Traits\Capacity;
+
+    const MIN_CAPACITY = 8;
+
+    /**
+     * @return float
+     */
+    protected function getGrowthFactor()
+    {
+        return 1.5;
+    }
+
+    /**
+     * @return bool whether capacity should be increased.
+     */
+    protected function shouldIncreaseCapacity()
+    {
+        return count($this) > $this->capacity;
+    }
+}
