c37bf3da2507921b81d562dcbedbc03b5c5a29f7
Danilo Andrade <mandrade.danilo@gmail.com>
2018-08-06 10:10:18 -0300

    add base classes for a queue collection

---
+++ src/Data/PriorityQueue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Danilo
+ * Date: 02/08/2018
+ * Time: 10:16
+ */
+
+namespace Apli\Data;
+
+
+class PriorityQueue
+{
+
+}
---
+++ src/Data/Queue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Danilo
+ * Date: 02/08/2018
+ * Time: 10:15
+ */
+
+namespace Apli\Data;
+
+
+class Queue
+{
+
+}
