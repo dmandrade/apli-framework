0ccb6a82e649a2a73ce9feccd09bae5d438c74ad
Danilo Andrade <mandrade.danilo@gmail.com>
2018-08-16 20:39:33 -0300

    a base Queue and PriorityQueue objects

--- src/Data/PriorityQueue.php
+++ src/Data/PriorityQueue.php
@@ -8,8 +8,82 @@
 
 namespace Apli\Data;
 
+use Traversable;
 
-class PriorityQueue
+/**
+ * PriorityQueue is like Queue but with an assigned priority, and the value
+ * with the highest priority will always be at the top of the queue.
+ *
+ * @package Apli\Data
+ */
+class PriorityQueue implements \IteratorAggregate, Collection
 {
+    use Traits\GenericCollection;
+    use Traits\SquaredCapacity;
 
+    /**
+     * @var int
+     */
+    const MIN_CAPACITY = 8;
+
+    /**
+     * @var array
+     */
+    private $heap = [];
+
+    /**
+     * @var int
+     */
+    private $stamp = 0;
+
+    /**
+     * Creates a new instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Removes all values from the collection.
+     */
+    function clear()
+    {
+        // TODO: Implement clear() method.
+    }
+
+    /**
+     * Returns the size of the collection.
+     *
+     * @return int
+     */
+    function count()
+    {
+        // TODO: Implement count() method.
+    }
+
+    /**
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     * @since 5.0.0
+     */
+    public function getIterator()
+    {
+        // TODO: Implement getIterator() method.
+    }
+
+    /**
+     * Returns an array representation of the collection.
+     *
+     * The format of the returned array is implementation-dependent. Some
+     * implementations may throw an exception if an array representation
+     * could not be created (for example when object are used as keys).
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        // TODO: Implement toArray() method.
+    }
 }
--- src/Data/Queue.php
+++ src/Data/Queue.php
@@ -9,7 +9,138 @@
 namespace Apli\Data;
 
 
-class Queue
-{
+use Traversable;
 
+/**
+ * Queue is a collection that only allows access to the top value
+ * of the queue and iterates in that order, destructively.
+ *
+ * @package Apli\Data
+ */
+class Queue implements \IteratorAggregate, \ArrayAccess, Collection
+{
+    use Traits\GenericCollection;
+
+    const MIN_CAPACITY = 8;
+
+    /**
+     * @var Deque internal deque to store values.
+     */
+    private $deque;
+
+    /**
+     * Creates an instance using the values of an array or Traversable object.
+     *
+     * @param array|\Traversable $values
+     */
+    public function __construct($values = null)
+    {
+        $this->deque = new Deque($values ?: []);
+    }
+
+    /**
+     * Removes all values from the collection.
+     */
+    function clear()
+    {
+        // TODO: Implement clear() method.
+    }
+
+    /**
+     * Returns the size of the collection.
+     *
+     * @return int
+     */
+    function count()
+    {
+        // TODO: Implement count() method.
+    }
+
+    /**
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     * @since 5.0.0
+     */
+    public function getIterator()
+    {
+        // TODO: Implement getIterator() method.
+    }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        // TODO: Implement offsetExists() method.
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        // TODO: Implement offsetGet() method.
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        // TODO: Implement offsetSet() method.
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        // TODO: Implement offsetUnset() method.
+    }
+
+    /**
+     * Returns an array representation of the collection.
+     *
+     * The format of the returned array is implementation-dependent. Some
+     * implementations may throw an exception if an array representation
+     * could not be created (for example when object are used as keys).
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        // TODO: Implement toArray() method.
+    }
 }
